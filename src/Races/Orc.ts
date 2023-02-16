329bf28ff2aa1d574f225a75f3061ff4e79cd7fd
Cristiane Kizelevicius <cristiane@samaan.com.br>
2023-02-16 16:17:19 -0300

    feat: Implementa a subClasse Orc

---
+++ src/Races/Orc.ts
@@ -0,0 +1,22 @@
+import Race from './Race';
+
+class Orc extends Race {
+  private _maxLifePoints: number;
+  private static _racesInstances = 0;
+
+  constructor(name: string, dexterity: number) {
+    super(name, dexterity);
+    this._maxLifePoints = 74;
+    Orc._racesInstances += 1;
+  }
+
+  static createdRacesInstances(): number {
+    return Orc._racesInstances;
+  }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+}
+
+export default Orc;
