75fac04ebb06907c0f00443fc88eec0315bc85db
Cristiane Kizelevicius <cristiane@samaan.com.br>
2023-02-16 16:13:34 -0300

    feat: Implementa a subClasse halfling

--- src/Races/index.ts
+++ src/Races/index.ts
@@ -1,7 +1,8 @@
 import Race from './Race';
 import Dwarf from './Dwarf';
 import Elf from './Elf';
+import Halfling from './Halfling';
 
-export { Dwarf, Elf };
+export { Dwarf, Elf, Halfling };
 
 export default Race;
